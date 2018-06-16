14482f0323c3f7cca7647909c873df1fd4ca2aee
Ruslan Akberov <ruslanakberov@gmail.com>
2018-06-16 15:48:07 +0300

    Custom transition implemented

--- CircularTransition/FirstVC.swift
+++ CircularTransition/FirstVC.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class FirstVC: UIViewController {
+class FirstVC: UIViewController, UIViewControllerTransitioningDelegate {
 
     @IBOutlet weak var warcraftButton: UIButton!
+    let transition = CircularTransition()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,38 @@ class FirstVC: UIViewController {
     func setupUI() {
         warcraftButton.layer.cornerRadius = warcraftButton.bounds.width / 2
     }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .present
+        transition.startingPoint = warcraftButton.center
+        transition.circleColor = warcraftButton.backgroundColor!
+        return transition
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .dismiss
+        transition.startingPoint = warcraftButton.center
+        transition.circleColor = warcraftButton.backgroundColor!
+        return transition
+    }
 
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondVC = segue.destination as! SecondVC
+        secondVC.transitioningDelegate = self
+        secondVC.modalPresentationStyle = .custom
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
